e5451948eeaeea921efea504a85a105533b806d6
unknown <adigunisrael@gmail.com>
2023-01-21 16:57:06 +0100

    Update JS file

--- app.js
+++ app.js
@@ -8,15 +8,7 @@ let input = document.querySelector('input'),
     ispValue = document.getElementById("isp");
 
 
-var map = L.map('map').setView([40.69371, -73.98596], 15);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([40.69371, -73.98596]).addTo(map)
-    .bindPopup('Downtown Brooklyn')
-    .openPopup();
+var map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 15});
 
 var popup = L.popup();
 
@@ -27,6 +19,12 @@ function onMapClick(e) {
         .openOn(map);
 }
 map.on('click', onMapClick);
+
+window.onload = function(){
+    map.off();
+    map.remove();
+    fetchInputIpAddress();
+};
 
 enterKey.addEventListener("click", function(){
     map.off();
@@ -45,7 +43,7 @@ input.addEventListener('keypress', function(e){
 function fetchInputIpAddress(){
     var url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_BtZR9zJi8z3lWCZA1dn9ihdAycOgp&ipAddress=';
     var urlInput = url + input.value;
-    fetch(urlInput)
+    fetch(urlInput, {method: 'GET'})
     .then(data => data.json())
     .then(loc => {
         var locs = loc.location;
@@ -72,3 +70,28 @@ function fetchInputIpAddress(){
         console.error(error);
     });
 }
+
+
+// var map = L.map('map').setView([40.69371, -73.98596], 15);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([40.69371, -73.98596]).addTo(map)
+//     .bindPopup('Downtown Brooklyn')
+//     .openPopup();
+
+// var latit = 0;
+// var longit = 0;
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     latit = position.coords.latitude;
+//     longit = position.coords.longitude;
+//     // this is just a marker placed in that position
+//     var abc = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+//     // move the map to have the location in its center
+//     map.panTo(new L.LatLng(latit, longit));
+//     map.panTo(abc.getLatLng()); //pass the marker's position to the panTo() function
+//   })
+// }
